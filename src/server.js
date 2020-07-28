212a4cfef02df9af3ece8fef4c1d1da38a3179de
edimario <edimariobabau@gmail.com>
2020-07-28 19:04:29 -0300

    cors origin

--- src/server.js
+++ src/server.js
@@ -6,10 +6,9 @@ import connection from "./db/config/connection";
 require("dotenv").config();
 
 const app = express();
-app.use(express.json())
-app.use(cors());
+app.use(express.json());
+app.use(cors({ origin: "https://cinebusca-frontend.herokuapp.com" }));
 app.use(routes);
-
 
 connection();
 
